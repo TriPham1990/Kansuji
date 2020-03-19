8544946d077368d8a658ab79a18f46d3decab59e
TriPham1990 <pmtri1990@gmail.com>
2020-03-19 13:34:34 +0700

    hoan thanh dem tu 0 den 10

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,1 +1,20 @@
 #TODO: implement
+
+class ConvertBetweenKanjiAndNumber
+
+  BASICKANJI = {
+    0 => "〇", 1 => "一", 2 => "二", 3 => "三", 4 => "四",
+    5 => "五", 6 => "六", 7 => "七", 8 => "八", 9 => "九",
+    10 => "十", 100 => "百"
+  }
+
+  def self.convert(number)
+    BASICKANJI[number]
+    # if number == 0
+    #   "〇"
+    # else
+    #   "一"
+    # end
+  end      
+
+end
--- spec/lib/kansuji_spec.rb
+++ spec/lib/kansuji_spec.rb
@@ -1,4 +1,50 @@
 require "spec_helper"
+require "kansuji"
 
 #TODO: TDD
 
+describe ConvertBetweenKanjiAndNumber do
+
+  describe ".convert" do
+    it 'return 〇 of 0' do
+      number = ConvertBetweenKanjiAndNumber.convert(0)
+      expect(number).to eq("〇")
+    end
+  end
+
+  describe ".convert" do
+    it 'return 一 of 1' do
+      number = ConvertBetweenKanjiAndNumber.convert(1)
+      expect(number).to eq("一")
+    end
+  end
+
+  describe ".convert" do
+    it 'return 三 of 3' do
+      number = ConvertBetweenKanjiAndNumber.convert(3)
+      expect(number).to eq("三")
+    end
+  end
+
+  describe ".convert" do
+    it 'return 七 of 7' do
+      number = ConvertBetweenKanjiAndNumber.convert(7)
+      expect(number).to eq("七")
+    end
+  end
+
+  describe ".convert" do
+    it 'return 十 of 10' do
+      number = ConvertBetweenKanjiAndNumber.convert(10)
+      expect(number).to eq("十")
+    end
+  end
+
+  describe ".convert" do
+    it 'return 十一 of 11' do
+      number = ConvertBetweenKanjiAndNumber.convert(11)
+      expect(number).to eq("十一")
+    end
+  end
+  
+end
